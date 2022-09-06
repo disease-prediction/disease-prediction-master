733917046fefa526e0e3fb03480b98dca7bbc4e7
disease-prediction <95067749+disease-prediction@users.noreply.github.com>
2022-09-06 16:16:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,14 +24,14 @@ stop_words = stopwords.words('english')
 lemmatizer = WordNetLemmatizer()
 splitter = RegexpTokenizer(r'\w+')
 app = Flask(__name__)
-df_comb = pd.read_csv("dataset/dis_sym_dataset_dr_comb.csv") # Disease combination
-X_train = df_comb.iloc[:, 1:]
-Y_train = df_comb.iloc[:, 0:1]
+# df_comb = pd.read_csv("dataset/dis_sym_dataset_dr_comb.csv") # Disease combination
+# X_train = df_comb.iloc[:, 1:]
+# Y_train = df_comb.iloc[:, 0:1]
 
-rf =   LogisticRegression()
-rf = rf.fit(X_train, Y_train)
-scores = cross_val_score(rf, X_train, Y_train, cv=3)
-score = round(Decimal(scores.mean()*100),2)
+# rf =   LogisticRegression()
+# rf = rf.fit(X_train, Y_train)
+# scores = cross_val_score(rf, X_train, Y_train, cv=3)
+# score = round(Decimal(scores.mean()*100),2)
 
 
 # test=pd.read_csv("dataset/dis_sym_dataset_dr_norm.csv",error_bad_lines=False)
